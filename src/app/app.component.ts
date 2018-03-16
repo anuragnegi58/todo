17463d77238a7c575cfa58c1587c87bfbde4fc9e
anurag.negi <anurag.negi@accenture.com>
2018-03-16 20:31:20 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {TodosComponent} from './todos/todos.component';
-import {TodoService} from './todo.service';
+import {TodoService} from './todo.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import { TodoService } from './todo.service';
+import { CreateComponent } from './create/create.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'create', component: CreateComponent,
+  },
+  {
+    path: 'todo', component: TodosComponent,
+  },
+  {
+    path: '', redirectTo: 'todo', pathMatch: 'full',
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
+    CreateComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [
     TodoService,
